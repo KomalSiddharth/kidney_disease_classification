df46194b55412255cb84d63a189996e315620911
KomalSiddharth <komalsiddharth814@gmail.com>
2024-01-27 18:14:00 +0530

    done

--- setup.py
+++ setup.py
@@ -1,22 +1,26 @@
 import setuptools
-with open("README.md","r",encoding="utf-8") as f:
-    long_description=f.read()
 
-__version__="0.0.0"
-REPO_NAME="kidney_disease_classification"
-AUTHOR_USER_NAME="KomalSiddharth"
-SRC_REPO="cnnClassifier"
-AUTHOR_EMAIL="Komalsiddharth814@gmail.com"
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+
+__version__ = "0.0.0"
+
+REPO_NAME = "Kidney-Disease-Classification-Deep-Learning-Project"
+AUTHOR_USER_NAME = "krishnaik06"
+SRC_REPO = "cnnClassifier"
+AUTHOR_EMAIL = "entbappy73@gmail.com"
+
 
 setuptools.setup(
     name=SRC_REPO,
     version=__version__,
     author=AUTHOR_USER_NAME,
     author_email=AUTHOR_EMAIL,
-    description="python package for cnn app",
+    description="A small python package for CNN app",
     long_description=long_description,
     long_description_content="text/markdown",
-    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}"
+    url=f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}",
     project_urls={
         "Bug Tracker": f"https://github.com/{AUTHOR_USER_NAME}/{REPO_NAME}/issues",
     },
